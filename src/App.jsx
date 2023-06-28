aefd46049a74c6d9f7b33a50afbc0f549385592c
fachuazulgrana <facundo.criscuolo@davinci.edu.ar>
2023-06-28 13:51:01 +0200

    Products API import, create component and layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,45 @@
 import "./App.css";
 import Header from "../src/components/header/index";
-import AddToCart from "./components/cart/itemListContainer";
+import Card from "./components/products/card";
+import { useState, useEffect } from 'react'
 
 function App() {
- 
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getProduct = async () => {
+      try {
+        const response = await fetch('https://6499986179fbe9bcf83f91bf.mockapi.io/products', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+
+        const data = await response.json();
+
+        setProducts(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getProduct();
+  }, [])
+
+
   return (
     <div>
       <Header logo="criscuolo.tech" />
+
+      <div className='cardContainer'>
+        {
+          products.map((product) => (
+            <Card {...product} />
+          ))
+        }
+        </div>
+
     </div>
   )
 }
